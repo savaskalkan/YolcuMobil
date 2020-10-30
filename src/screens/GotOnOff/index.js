651eab3ef5e9f289d343d7075482cf5aa3aff882
Savas Kalkan <savaskalkan@Savas-MacBook-Pro-2.local>
2020-10-30 17:02:44 +0300

    base 64 to string

--- src/screens/GotOnOff/index.js
+++ src/screens/GotOnOff/index.js
@@ -41,15 +41,15 @@ export default function App() {
   qrService = new QrService();
 
   const setQrService = (data) => {
-    console.log("location",location)
-    console.log("data",data)
+    console.log("location", location)
+    console.log("data", data)
     this.qrService.setQrInfo(data).then(responseJson => {
       console.log("responseJson", responseJson)
       if (responseJson.IsSuccess) {
-        Alert.alert("Yolcu Mobil","Bilgiler başarıyla kaydedildi..!")
+        Alert.alert("Yolcu Mobil", "Bilgiler başarıyla kaydedildi..!")
         return;
-      }else{
-        Alert.alert("Yolcu Mobil","Bilgiler kaydedilirken hata ile karşılaşıldı..!\n" + responseJson.ExceptionMsg )
+      } else {
+        Alert.alert("Yolcu Mobil", "Bilgiler kaydedilirken hata ile karşılaşıldı..!\n" + responseJson.ExceptionMsg)
       }
 
     }).catch((error) => {
@@ -61,7 +61,7 @@ export default function App() {
     (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
       setHasPermission(status === 'granted');
-      
+
     })();
   }, []);
 
@@ -76,12 +76,14 @@ export default function App() {
       setLocation(location);
     })();
   }, []);
-  
-  const handleBarCodeScanned = ({ type, data }) => {
+
+  const handleBarCodeScanned = ({ type, qrdata }) => {
     setIsCancel(false)
     setScanned(true);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
-
+    // Decode the String
+    var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+    const data = Base64.decode(qrdata); 
+    alert(`Bar code with type ${type} and data ${qrdata} has been scanned!-> ${data}`);
     AsyncStorage.getItem(StorageKeys.PassengerDetailKey).then(userinfo => {
       userinfo = JSON.parse(userinfo)
 
